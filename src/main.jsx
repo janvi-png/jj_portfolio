b3a63894a061b91262001e92120216e0e22f2f3d
janvi-png <sharmajjjanvi@gmail.com>
2025-10-23 19:21:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react'; 
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+
+    {/* Analysis components before the closing tag */}
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
